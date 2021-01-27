61f95980e9d057dab80ca546e8dcc19429d9e72e
Mirza Chilman <mirzachilman@gmail.com>
2021-01-27 20:42:33 +0700

    add base app event

--- src/containers/PokeListContainer.tsx
+++ src/containers/PokeListContainer.tsx
@@ -1,12 +1,26 @@
 import { useState } from "react";
 import { createContainer } from "unstated-next";
+import BaseAppEvent from "../utils/BaseAppEvent";
+
+
+type EventTypes = {
+  success: string;
+}
 
 const Container = () => {
   const [testState, setTestState] = useState<string>("asdas");
+  const event = BaseAppEvent<EventTypes>()
+
+  const tryBaseEvent  = () => {
+    event.emit("success", "It is a big success")
+  }
+
 
   return {
     testState,
     setTestState,
+    tryBaseEvent,
+    event
   };
 };
 

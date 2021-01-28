6b61551a5f21ea8a4f437d14cbfa431c3349ba56
Mirza Chilman <mirzachilman@gmail.com>
2021-01-28 08:52:06 +0700

    improve webpack and delete antd, its freaking hugeeeeeee UI library ._.

--- configs/webpack.prod.js
+++ configs/webpack.prod.js
@@ -1,16 +1,131 @@
-const { merge } = require("webpack-merge");
-const { resolve } = require("path");
+// Important modules this config uses
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const OfflinePlugin = require("offline-plugin");
+const {
+  ids: { HashedModuleIdsPlugin },
+} = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
 
-const webpackBase = require("./webpack.base");
+module.exports = require("./webpack.base.js")({
+  mode: "production",
 
-module.exports = merge(webpackBase, {
-    mode: "production",
-    entry: "./index.tsx",
-    output: {
-        filename: "js/bundle.[contenthash].min.js",
-        path: resolve(__dirname, "../dist"),
-        publicPath: "/",
+  // In production, we skip all hot-reloading stuff
+  entry: [
+    require.resolve("react-app-polyfill/ie11"),
+    path.join(process.cwd(), "src/index.tsx"),
+  ],
+
+  // Utilize long-term caching by adding content hashes (not compilation hashes) to compiled assets
+  output: {
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[name].[chunkhash].chunk.js",
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          warnings: false,
+          compress: {
+            comparisons: false,
+          },
+          parse: {},
+          mangle: true,
+          output: {
+            comments: false,
+            ascii_only: true,
+          },
+        },
+        parallel: true,
+      }),
+    ],
+    nodeEnv: "production",
+    sideEffects: true,
+    concatenateModules: true,
+    runtimeChunk: "single",
+    splitChunks: {
+      chunks: "all",
+      maxInitialRequests: 10,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+            return `npm.${packageName.replace("@", "")}`;
+          },
+        },
+      },
     },
-    devtool: "source-map",
-    plugins: [],
+  },
+
+  plugins: [
+    // Minify and optimize the index.html
+    new HtmlWebpackPlugin({
+      template: "src/public/index.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+      inject: true,
+    }),
+
+    // Put it in the end to capture all the HtmlWebpackPlugin's
+    // assets manipulations and do leak its manipulations to HtmlWebpackPlugin
+
+    new CompressionPlugin({
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+
+    new WebpackPwaManifest({
+      name: "Pokelist",
+      short_name: "Pokelist",
+      description: "Pokelist!",
+      background_color: "#fafafa",
+      theme_color: "#b1624d",
+      inject: true,
+      ios: true,
+      // icons: [
+      //   {
+      //     src: path.resolve("app/images/icon-512x512.png"),
+      //     sizes: [72, 96, 128, 144, 192, 384, 512],
+      //   },
+      //   {
+      //     src: path.resolve("app/images/icon-512x512.png"),
+      //     sizes: [120, 152, 167, 180],
+      //     ios: true,
+      //   },
+      // ],
+    }),
+
+    new HashedModuleIdsPlugin({
+      hashFunction: "sha256",
+      hashDigest: "hex",
+      hashDigestLength: 20,
+    }),
+
+
+  ],
+
+  performance: {
+    assetFilter: (assetFilename) =>
+      !/(\.map$)|(^(main\.|favicon\.))/.test(assetFilename),
+  },
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,22 @@
 import * as React from "react";
 import { hot } from "react-hot-loader";
-import { Button, Layout, Row, Col, notification } from "antd";
 import PokeListContainer from "./containers/PokeListContainer";
 
 const App = () => {
   const pokeListContainer = PokeListContainer.useContainer();
   const { testState, setTestState, tryBaseEvent, event } = pokeListContainer;
 
-  React.useEffect(() => {
-    event.subscribe("success", (msg) => notification.success({ message: msg }));
-    return () =>
-      event.unsubscribe("success", (msg) =>
-        notification.success({ message: msg })
-      );
-  }, [event]);
-
   return (
     <React.Suspense fallback={"loading"}>
-      <Layout>
-        {testState}
-        <Row>
-          <Col>
-            <Button
-              onClick={() => {
-                setTestState(String(Math.random()));
-                tryBaseEvent();
-              }}
-            >
-              Test Button
-            </Button>
-          </Col>
-        </Row>
-      </Layout>
+      {testState}
+      <button
+        onClick={() => {
+          setTestState(String(Math.random()));
+          tryBaseEvent();
+        }}
+      >
+        Test Button
+      </button>
     </React.Suspense>
   );
 };

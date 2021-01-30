5afa8372538302fc3687a3656f7165aff1d793ab
Mirza Chilman <mirzachilman@gmail.com>
2021-01-30 13:03:05 +0700

    try once again

--- configs/webpack.prod.js
+++ configs/webpack.prod.js
@@ -102,11 +102,11 @@ module.exports = require("./webpack.base.js")({
       ios: true,
       icons: [
         {
-          src: path.resolve("public/assets/icon-512x512.png"),
+          src: path.resolve("src/assets/android-chrome-512x512.png"),
           sizes: [72, 96, 128, 144, 192, 384, 512],
         },
         {
-          src: path.resolve("public/assets/icon-512x512.png"),
+          src: path.resolve("src/assets/android-chrome-512x512.png"),
           sizes: [120, 152, 167, 180],
           ios: true,
         },
